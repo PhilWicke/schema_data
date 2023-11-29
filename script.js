960b5a5603fd3df0f837a4c8580cb838fb0def43
PhilWicke <philipp.wicke@ucdconnect.ie>
2023-11-29 18:04:18 +0100

    update 1

--- script.js
+++ script.js
@@ -1,58 +1,25 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const mainImage = document.getElementById("mainImage");
-    const conditionSelector = document.getElementById("conditionSelector");
-    const modelSelector = document.getElementById("modelSelector");
+function updateModels() {
+    var condition = document.getElementById("condition").value;
+    var modelDropdown = document.getElementById("model");
 
-    const imageFolder = "images/";
+    modelDropdown.innerHTML = "";
 
-    function updateImage() {
-        const condition = conditionSelector.value;
-        const model = modelSelector.value;
-        const imagePath = `${imageFolder}${condition}_${model}_Choices.png`;
-        mainImage.src = imagePath;
-    }
-
-    function populateModelOptions() {
-        const condition = conditionSelector.value;
-        const models = getModelsForCondition(condition);
-
-        modelSelector.innerHTML = ""; // Clear previous options
-
-        models.forEach((model) => {
-            const option = document.createElement("option");
-            option.value = model;
-            option.text = model;
-            modelSelector.add(option);
-        });
-
-        updateImage(); // Update the image when the models are populated
-    }
-
-    function getModelsForCondition(condition) {
-        const modelSet = new Set();
-
-        // Fetch models based on the condition
-        fetch(`${imageFolder}${condition}/`)
-            .then(response => response.text())
-            .then(data => {
-                const parser = new DOMParser();
-                const htmlDoc = parser.parseFromString(data, "text/html");
-                const files = Array.from(htmlDoc.querySelectorAll("a"))
-                    .map(link => link.getAttribute("href"))
-                    .filter(file => file.endsWith("_Choices.png"));
-
-                files.forEach(file => {
-                    const model = file.split("_")[1];
-                    modelSet.add(model);
-                });
-
-                // Trigger the model options update
-                populateModelOptions();
+    fetch(`images/${condition}`)
+        .then(response => response.json())
+        .then(models => {
+            models.forEach(model => {
+                var option = document.createElement("option");
+                option.text = model;
+                modelDropdown.add(option);
             });
+        });
+}
 
-        return Array.from(modelSet);
-    }
+updateModels();
 
-    conditionSelector.addEventListener("change", populateModelOptions);
-    modelSelector.addEventListener("change", updateImage);
+document.getElementById("model").addEventListener("change", function () {
+    var condition = document.getElementById("condition").value;
+    var model = this.value;
+    var imagePath = `images/${condition}/${condition}_${model}_Choices.png`;
+    document.getElementById("selectedImage").src = imagePath;
 });
