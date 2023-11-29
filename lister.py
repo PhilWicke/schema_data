f7e0ecb67a4c2bf35f052146f95ac97a6398d777
PhilWicke <philipp.wicke@ucdconnect.ie>
2023-11-29 20:50:42 +0100

    update 5

--- lister.py
+++ lister.py
@@ -9,3 +9,20 @@ models = {condition: [extract_model_name(file) for file in os.listdir(os.path.jo
 
 for condition, model_list in models.items():
     print(f'{condition}: {model_list}')
+
+
+# define a variable "cut"
+cut = 210
+# cut the same amount off the top and bottom of each image
+# it should be a total of 400 pixels
+
+from PIL import Image
+
+for condition, model_list in models.items():
+    for model in model_list:
+        image = Image.open(os.path.join(images_folder, condition, f'{condition}_{model}_Choices.png'))
+        width, height = image.size
+        image = image.crop((0, cut, width, height - cut))
+        image.save(os.path.join(images_folder, condition, f'{condition}_{model}_Choices.png'))
+        print(f'{condition}_{model}_Choices.png has been cropped and saved')
+
